f7a7b37033493938292c453cf199f71565620e83
marcolcu <marcomelandri808@gmail.com>
2024-02-19 21:53:58 +0700

    [UPDATE] Membuat loading, membuat contoh card, dan smooth scrolling

--- src/app/landing/LandingPage.jsx
+++ src/app/landing/LandingPage.jsx
@@ -1,13 +1,31 @@
 "use client";
-import React, { useEffect } from "react";
-import BannerPage from "./BannerPage";
-import CurrentFav from "./CurrentFav";
+import React, { useEffect, useState } from "react";
+import BannerPage from "./section/BannerPage";
+import CurrentFav from "./section/CurrentFav";
+import SkeletonLoading from "./section/SkeletonLoading";
 
 const LandingPage = () => {
+  const [loading, setLoading] = useState(true);
+
+  // Simulate a delay to showcase the loading skeleton
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="max-w-screen-xl mx-auto px-10" style={{ height: "1000px" }}>
-      <BannerPage />
-      <CurrentFav />
+      {loading ? (
+        <SkeletonLoading />
+      ) : (
+        <>
+          <BannerPage />
+          <CurrentFav />
+        </>
+      )}
     </div>
   );
 };
