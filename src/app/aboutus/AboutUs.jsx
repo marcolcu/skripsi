1f3e64645d068c1a26985decf8751c60d99232fb
marcolcu <marcomelandri808@gmail.com>
2024-04-29 21:12:48 +0700

    [UPDATE] class -> className

--- src/app/aboutus/AboutUs.jsx
+++ src/app/aboutus/AboutUs.jsx
@@ -22,33 +22,33 @@ const AboutUs = () => {
           Man behind the apps
         </p>
 
-        <div class="flex my-8">
-          <div class="w-1/3 flex flex-col items-center mr-4">
+        <div className="flex my-8">
+          <div className="w-1/3 flex flex-col items-center mr-4">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/eventappskripsi.appspot.com/o/0b6e7329-04e4-4f60-ad8a-142858f9b896.jpeg?alt=media"
               alt=""
-              class="w-32 h-32 object-cover rounded-full"
+              className="w-32 h-32 object-cover rounded-full"
             />
-            <p class="text-center mt-5 font-bold">Oliver Oswin Sumady</p>
-            <p class="text-center mt-2 italic">Backend Programmer</p>
+            <p className="text-center mt-5 font-bold">Oliver Oswin Sumady</p>
+            <p className="text-center mt-2 italic">Backend Programmer</p>
           </div>
-          <div class="w-1/3 flex flex-col items-center mr-4">
+          <div className="w-1/3 flex flex-col items-center mr-4">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/eventappskripsi.appspot.com/o/0f1f4419-50cc-4d36-95a7-9203fc83e257.jpg?alt=media"
               alt=""
-              class="w-32 h-32 object-cover rounded-full"
+              className="w-32 h-32 object-cover rounded-full"
             />
-            <p class="text-center mt-5 font-bold">Brian Joe Antoni</p>
-            <p class="text-center mt-2 italic">Frontend Programmer</p>
+            <p className="text-center mt-5 font-bold">Brian Joe Antoni</p>
+            <p className="text-center mt-2 italic">Frontend Programmer</p>
           </div>
-          <div class="w-1/3 flex flex-col items-center">
+          <div className="w-1/3 flex flex-col items-center">
             <img
               src="https://firebasestorage.googleapis.com/v0/b/eventappskripsi.appspot.com/o/10113054-e994-4484-ac34-ae829482bb29.jpg?alt=media"
               alt=""
-              class="w-32 h-32 object-cover rounded-full"
+              className="w-32 h-32 object-cover rounded-full"
             />
-            <p class="text-center mt-5 font-bold">Vincentius Marco Melandri</p>
-            <p class="text-center mt-2 italic">Frontend Programmer</p>
+            <p className="text-center mt-5 font-bold">Vincentius Marco Melandri</p>
+            <p className="text-center mt-2 italic">Frontend Programmer</p>
           </div>
         </div>
       </div>
