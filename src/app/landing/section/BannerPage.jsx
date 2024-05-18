295e698266f7346795a326b7ebc7772da4cb85c4
Vincentius Marco Melandri <103645926+marcolcu@users.noreply.github.com>
2024-05-18 15:04:12 +0700

    Update BannerPage.jsx

--- src/app/landing/section/BannerPage.jsx
+++ src/app/landing/section/BannerPage.jsx
@@ -5,7 +5,7 @@ const BannerPage = () => {
   return (
     <main>
       <img
-        src="https://thebaliguideline.com/build/event/dwp-xv.jpg"
+        src="https://firebasestorage.googleapis.com/v0/b/eventappskripsi.appspot.com/o/a58ac0bd-2b9c-4d42-8e00-fea00da3503b.png?alt=media"
         alt="Deskripsi gambar"
         className="w-full h-auto rounded-[2rem]"
       />
