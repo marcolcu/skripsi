6bcbafd0628c1889d53fc387fdcdadf7042ca5a8
Vincentius Marco Melandri <103645926+marcolcu@users.noreply.github.com>
2024-05-18 15:13:00 +0700

    Update BannerPage.jsx

--- src/app/landing/section/BannerPage.jsx
+++ src/app/landing/section/BannerPage.jsx
@@ -7,7 +7,7 @@ const BannerPage = () => {
       <img
         src="https://firebasestorage.googleapis.com/v0/b/eventappskripsi.appspot.com/o/a58ac0bd-2b9c-4d42-8e00-fea00da3503b.png?alt=media"
         alt="Deskripsi gambar"
-        className="w-full h-auto rounded-[2rem]"
+        className="w-full h-auto rounded-[2rem] mb-5"
       />
     </main>
   );
