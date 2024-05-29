69ef860b2f8b5425a80200b6f20cc84166b57603
marcolcu <marcomelandri808@gmail.com>
2024-05-29 12:02:20 +0700

    [UPDATE] Revisi

--- src/app/venue/VenueList.jsx
+++ src/app/venue/VenueList.jsx
@@ -120,7 +120,7 @@ const VenueList = () => {
 
   const generateTimeOptions = () => {
     const times = [];
-    for (let hour = 0; hour < 24; hour++) {
+    for (let hour = 5; hour < 24; hour++) {
       for (let minute = 0; minute < 60; minute += 15) {
         const hourString = hour.toString().padStart(2, "0");
         const minuteString = minute.toString().padStart(2, "0");
--- src/app/venue/[slug]/VenueDetail.jsx
+++ src/app/venue/[slug]/VenueDetail.jsx
@@ -162,7 +162,7 @@ const VenueDetailPage = ({ slug }) => {
 
   const generateTimeOptions = () => {
     const times = [];
-    for (let hour = 0; hour < 24; hour++) {
+    for (let hour = 5; hour < 24; hour++) {
       for (let minute = 0; minute < 60; minute += 15) {
         const hourString = hour.toString().padStart(2, "0");
         const minuteString = minute.toString().padStart(2, "0");
